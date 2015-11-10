67525f4e6eb1dd0f95d7818cc5a2a8e399a4ab03
Sevag Antranik <darknight-90@hotmail.com>
2015-11-10 20:58:56 +0100

    user profile new design

--- MemoriesApplication/MemoriesApplication/app/src/main/java/com/meeladsd/memoriesapplication/MyProfileactivity2.java
+++ MemoriesApplication/MemoriesApplication/app/src/main/java/com/meeladsd/memoriesapplication/MyProfileactivity2.java
@@ -6,7 +6,12 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -25,21 +30,38 @@ public class MyProfileactivity2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profileactivity2);
-        TextView textView_name ,textView_Freinds, txtViewVacations ;
-        textView_name =(TextView)findViewById(R.id.textView_name);
-        textView_Freinds = (TextView)findViewById(R.id.txtFriends);
-        txtViewVacations = (TextView)findViewById(R.id.vacationsView);
+        TextView textView_name, textView_Freinds, txtViewVacations;
+        textView_name = (TextView) findViewById(R.id.textView_name);
+        textView_Freinds = (TextView) findViewById(R.id.friends_item_counter);
+        txtViewVacations = (TextView) findViewById(R.id.vacations_item_counter);
+
+      getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
 
-        new MyProfile1(MyProfileactivity2.this,textView_name,textView_Freinds,txtViewVacations).execute();
+
+        // Enable the Up button
+
+        new MyProfile1(MyProfileactivity2.this, textView_name, textView_Freinds, txtViewVacations).execute();
 
 
     }
 
-    public void onBackPressed() {
-        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(intent);
-        finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main2, menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
